6e8aeb753a3139edb8dc456ad0bd66a99bff9acd
Tarshit Singh Rana <tarshitsinghrana@gmail.com>
2026-08-24 10:46:02 +0530

    Time: 97 ms (84.92%), Space: 91.8 MB (65.59%) - LeetHub

---
+++ 1872-stone-game-viii/1872-stone-game-viii.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int stoneGameVIII(vector<int>& stones) {
+        int n = stones.size();
+
+        // prefix[i] = sum of stones[0 ... i]
+        vector<int> prefix(n);
+        prefix[0] = stones[0];
+
+        for (int i = 1; i < n; i++) {
+            prefix[i] = prefix[i - 1] + stones[i];
+        }
+
+        // dp represents the best score difference
+        // starting from this state.
+        int dp = prefix[n - 1];
+
+        for (int i = n - 2; i >= 1; i--) {
+            dp = max(dp, prefix[i] - dp);
+        }
+
+        return dp;
+    }
+};
