810c59f657f0b31aff66cc9ea88f4ade689ec3e0
Tarshit Singh Rana <tarshitsinghrana@gmail.com>
2026-09-10 22:44:18 +0530

    Time: 0 ms (100%), Space: 11.8 MB (6.34%) - LeetHub

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
@@ -7,16 +7,16 @@ public:
         int temp = 0;
         bool ans = false;
         for(int i = 0; i<n;i++){
+            if(duplicate == nums){
+                ans = true;
+                break;
+            }
             temp = duplicate[0];
             for(int j = 0; j<n-1;j++){
                     duplicate[j] = duplicate[j+1]; 
             }
             duplicate[n-1] = temp;
             
-            if(duplicate == nums){
-                ans = true;
-                break;
-            }
         }
         if(ans == true){
             return true;
